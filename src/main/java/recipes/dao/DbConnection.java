24bce3992f150071c02376161d961af6c685c9be
Matt B <mattbaars@me.com>
2025-06-16 20:51:05 -0500

    edited DbConnection

--- src/main/java/recipes/dao/DbConnection.java
+++ src/main/java/recipes/dao/DbConnection.java
@@ -25,7 +25,7 @@ public class DbConnection {
 		try {
 			Connection conn = DriverManager.getConnection(uri);
 			System.out.println("Using connection string " + uri);
-			System.out.println("Connection to schema '" + SCHEMA + "' is successful!  Good job. Way to go Matt!");
+			System.out.println("Connection to schema '" + SCHEMA + "' is successful!  Good job.  Way to go Matt!  Outstanding!");
 			return conn;
 		} catch (SQLException e) {
 			System.out.println("Unable to get connection at " + uri);
